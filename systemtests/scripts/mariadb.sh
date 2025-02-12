1c3fa38bd04940b376d9577afce4928e0e5caf9a
Bruno Friedmann <bruno.friedmann@bareos.com>
2025-02-12 16:29:33 +0100

    [wip]: moving to mariadb pure

--- systemtests/scripts/mariadb.sh
+++ systemtests/scripts/mariadb.sh
@@ -38,8 +38,8 @@ mariadbd_cleanup()
 {
     mariadbd_server_stop
 
-    rm -Rf mysql/data/*
-    mkdir -p mysql/data/
+    rm -Rf mariadb/data/*
+    mkdir -p mariadb/data/
     # directory for socket, depending on length (socket length is limited)
     rm -Rf ${dbHost}
     mkdir -p ${dbHost}
@@ -55,46 +55,46 @@ mariadbd_init()
     # initialize mariadb db
     if ${MARIADB_DAEMON_BINARY} --verbose --help | grep -q initialize-insecure; then
         echo "MariaDB init with --initialize-insecure"
-        ${MARIADB_DAEMON_BINARY} --defaults-file=mariadbdefaults --initialize-insecure --user="${USER}" 2>&1 | tee mysql/mariadb_init.log
+        ${MARIADB_DAEMON_BINARY} --defaults-file=mariadbdefaults --initialize-insecure --user="${USER}" 2>&1 | tee mariadb/mariadb_init.log
         {
             echo "[client]"
-            echo "socket=${dbHost}/mysql.sock"
+            echo "socket=${dbHost}/mariadb.sock"
             echo "user=root"
         } > my.cnf
-        MYSQL_CLIENT="${MARIADB_CLIENT_BINARY}} --defaults-file=my.cnf"
+        MARIADB_CLIENT="${MARIADB_CLIENT_BINARY}} --defaults-file=my.cnf"
     else
         echo "MariaDBL init with ${MARIADB_INSTALL_DB_SCRIPT}"
-        ${MARIADB_INSTALL_DB_SCRIPT} --auth-root-authentication-method=socket --user="${USER}" --auth-root-socket-user="${USER}" --defaults-file=mysqldefaults > mysql/mysql_init.log
+        ${MARIADB_INSTALL_DB_SCRIPT} --auth-root-authentication-method=socket --user="${USER}" --auth-root-socket-user="${USER}" --defaults-file=mariadbdefaults > mariadb/mariadb_init.log
         {
             echo "[client]"
-            echo "socket=${dbHost}/mysql.sock"
+            echo "socket=${dbHost}/mariadb.sock"
             echo "user=${USER}"
         } > my.cnf
-        MYSQL_CLIENT="${MARIADB_CLIENT_BINARY}} --defaults-file=my.cnf"
+        MARIADB_CLIENT="${MARIADB_CLIENT_BINARY}} --defaults-file=my.cnf"
     fi
 }
 
-mysql_server_start()
+mariadb_server_start()
 {
-    if ! [ "${MYSQL_CLIENT:-}" ]; then
-        echo "MYSQL_CLIENT not defined. mysql_init not called?"
+    if ! [ "${MARIADB_CLIENT:-}" ]; then
+        echo "MARIADB_CLIENT not defined. mariadb_init not called?"
         return 1
     fi
 
     if ! [ -e my.cnf ]; then
-        echo "Config file 'my.cnf' does not exist. mysql_init not called?"
+        echo "Config file 'my.cnf' does not exist. mariadb_init not called?"
         return 1
     fi
 
-    "${MYSQL_DAEMON_BINARY}" --defaults-file=mysqldefaults >mysql/mysql.log 2>&1 &
+    "${MARIADB_DAEMON_BINARY}" --defaults-file=mariaddefaults > mariadb/mariadbd.log 2>&1 &
 
     tries=60
-    printf "waiting for MySQL server to start "
+    printf "waiting for Mariadbd server to start "
     while ! ${MARIADB_CLIENT} <<< "select version();" >/dev/null 2>&1; do
         [ $((tries-=1)) -eq 0 ] && {
-            echo "Could not start MySQL server"
-            cat mysql/mysql.log
-            mysql_cleanup
+            echo "Could not start Mariadbd server"
+            cat mariadb/mariadbd.log
+            mariadb_cleanup
             exit 1
         }
         printf "."
@@ -103,17 +103,17 @@ mysql_server_start()
     printf " OK\n"
 }
 
-get_mysql_server_pid()
+get_mariadb_server_pid()
 {
-    if [ -f "mysql/mysqld.pid" ]; then
-        cat "mysql/mysqld.pid"
+    if [ -f "mariadb/mariadb.pid" ]; then
+        cat "mariadb/mariadb.pid"
     fi
 }
 
-mysql_server_stop()
+mariadb_server_stop()
 {
-    printf "shutdown MySQL server "
-    local PID=$(get_mysql_server_pid)
+    printf "shutdown Mariadbd server "
+    local PID=$(get_mariadb_server_pid)
     if [ "$PID" ]; then
         kill $PID
         waitpid $PID
